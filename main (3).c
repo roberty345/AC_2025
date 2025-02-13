1a9aa16f75cb9b0e35070cef3048a3342c38f69d
roberty345 <robertyrodrigo2@gmail.com>
2025-02-13 15:51:48 +0000

    Add files via upload

---
+++ main (3).c
@@ -0,0 +1,7 @@
+1)R:Phishing.
+
+2)R:Alguns indicadores comuns de phishing incluem comunicações inesperadas que solicitam informações pessoais ou financeiras, endereços de correio eletrónico de remetentes desconhecidos, saudações genéricas, erros ortográficos e gramaticais e URLs enganadores.
+
+3)R:Não confie nos nomes a apresentar,Verifique se existem gralhas,Tenha cuidado com as ameaças,Reveja a assinatura.
+
+4)R:Comunicar com seu banco para cancelar as transferencias e congelar a conta.
